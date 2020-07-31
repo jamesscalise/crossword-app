3ee16ab2d6bdd26ee3bbfe1171182c12ad2be760
James <jamescscalise@gmail.com>
2020-07-31 18:10:38 -0400

    add fetch comments action

---
+++ frontend/src/actions/commentActions.js
@@ -0,0 +1,14 @@
+export const fetchComments = () => {
+    
+    return (dispatch) => {
+      
+      dispatch({ type: 'LOADING_COMMENTS'})
+      fetch('http://localhost:3001/comments').then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        
+        dispatch({ type: 'ADD_COMMENTS', scores: responseJSON })
+
+      })
+    }
+}
