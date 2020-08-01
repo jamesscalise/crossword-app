6835169f9dba13d7c4248b859005db01673040d2
James <jamescscalise@gmail.com>
2020-08-01 14:12:28 -0400

    Add empty comment form

---
+++ frontend/src/components/commentForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class commentForm extends Component {
+
+    state = {
+        content: ''
+    }
+
+    render (){
+        return(
+            <div></div>
+        )
+    }
+}  
+
+export default commentFrom
