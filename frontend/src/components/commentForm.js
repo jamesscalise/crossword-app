2729095c5596111f62b9db4d7c6c9acc66bd4694
James <jamescscalise@gmail.com>
2020-08-15 19:03:54 -0500

    flesh out comment form

--- frontend/src/components/commentForm.js
+++ frontend/src/components/commentForm.js
@@ -3,12 +3,32 @@ import React, { Component } from 'react';
 class commentForm extends Component {
 
     state = {
-        content: ''
+        text: ''
+    }
+
+    handleOnChange = event => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    handleOnSubmit(event) {
+        event.preventDefault();
+       // this.props.handleAddUser(this.state.text)
+        this.setState({
+          text: ''
+        })
     }
 
     render (){
         return(
-            <div></div>
+            <input 
+            name="name"
+            type="text"
+            value={this.state.text}
+           
+            onChange = {this.handleOnChange}
+          />
         )
     }
 }  
